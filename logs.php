764ef8a38bf389ca067ff99b5fe5d9131a39173a
Nicolas LEGAL <N.legal@P210.sn>
2024-05-23 16:36:37 +0200

    ajout gestion user (pas fonctionnel)

--- logs.php
+++ logs.php
@@ -33,11 +33,15 @@
                       <li class="nav-item">
                         <a class="nav-link" href="logs.php">Logs</a>
                       </li>
-                      <?php if (isset($_SESSION['type_user']) && $_SESSION['type_user'] === 'A') : ?>
-                            <li id="gestionUser" class="nav-item">
-                                <a class="nav-link" href="gestionutilisateurs.php">Gestion utilisateurs</a>
+                      <li id="gestionUser" class="nav-item">
+                              <?php
+                              if($_SESSION['type_users'] == 'A'){
+                                echo "<a class=\"nav-link\" href=\"gestionutilisateurs.php\"  >Gestion utilisateurs</a>";
+                              }
+                              ?>
                             </li>
-                        <?php endif; ?>
+                       </ul>
+                       
                     </ul>
                   </div>
                 </div>
