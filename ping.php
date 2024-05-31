d40f81c7ab26984c67a5be74d3464cd1a8c79327
Nicolas LEGAL <N.legal@P210.sn>
2024-05-31 16:36:30 +0200

    Ping V2

--- ping.php
+++ ping.php
@@ -1,10 +1,13 @@
 <?php
 if (isset($_GET['ip'])) {
-    $ipAddress = $_GET['ip'];
+    $ipAddress = escapeshellarg($_GET['ip']);
     // Exécutez la commande ping
-    exec("ping -n 4 $ipAddress", $output, $status); // Utilisation de -n au lieu de -c pour Windows
-    // Renvoie le résultat
-    echo implode("<br>", $output);
+    exec("ping -n 4 $ipAddress", $output, $status); // Utilisation de -c pour Linux, utilisez -n pour Windows
+    $response = htmlspecialchars(implode(" ", $output), ENT_QUOTES, 'UTF-8');
+    if ($status === 0) {
+        echo json_encode(['status' => 'Online', 'response' => $response]);
+    } else {
+        echo json_encode(['status' => 'Offline', 'response' => $response]);
+    }
 } 
-    
 ?>
